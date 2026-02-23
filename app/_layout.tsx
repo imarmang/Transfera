84dda8a8405e785bf63fc83fc11e01c9b917a75f
Arman Gasparyan <imarmang@Armans-MacBook-Pro.local>
2026-02-22 23:33:18 -0500

    Adding ctx.tsx, splash.tsx, useStorageState.ts, which saves, removes, and restores the suer session which would identify if the user needs to sign in or get to the Home Page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
 import { Stack } from "expo-router";
+import { SessionProvider, useSession } from "@/ctx";
+import { SplashScreenController } from "@/splash";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SessionProvider>
+      <SplashScreenController />
+      <RootNavigator />
+    </SessionProvider>
+  );
+}
+
+function RootNavigator() {
+  const { session } = useSession();
+
+  return (
+    <Stack>
+      {/* Authenticated Area */}
+
+      {/* Logged-Out Area */}
+    </Stack>
+  );
 }
---
+++ ctx.tsx
@@ -0,0 +1,43 @@
+import { createContext, PropsWithChildren, use } from "react";
+import { useStorageState } from "@/useStorageState";
+
+const AuthContext = createContext<{
+  signIn: () => void;
+  signOut: () => void;
+  session?: string | null;
+  isLoading: boolean;
+}>({
+  signIn: () => null,
+  signOut: () => null,
+  session: null,
+  isLoading: false,
+});
+
+export function useSession() {
+  const value = use(AuthContext);
+  if (!value)
+    throw new Error("useSession must be wrapped in <SessionProvider />");
+  return value;
+}
+
+export function SessionProvider({ children }: PropsWithChildren) {
+  const [[isLoading, session], setSession] = useStorageState("session");
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signIn: () => {
+          // mock session for now
+          setSession("xxx");
+        },
+        signOut: () => {
+          setSession(null);
+        },
+        session,
+        isLoading,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
